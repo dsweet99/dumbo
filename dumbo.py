caf2ef1875cd3492340ca9ee1b52bfced503bc60
David Sweet <dsweet@andamooka.org>
2023-02-20 13:07:29 -0500

    more in notebook; high-dim stuff

--- dumbo.py
+++ dumbo.py
@@ -16,11 +16,11 @@
 import numpy as np
 
 
-def acquisition_function(y_ex, y_var):
+def acquisition_function(y_ex, y_se):
     """Upper Confidence Bound (UCB)
     Seek points that have high value (y_ex) but also
-     high uncertainty (y_var).
-    Put another way: Balance exploration (y_var) with
+     high uncertainty (y_se).
+    Put another way: Balance exploration (y_se) with
      exploitation (y_ex).
     Biasing the next measurement towards areas where
      the surrogate is uncertain makes us take a measurement
@@ -31,7 +31,7 @@ def acquisition_function(y_ex, y_var):
      we divide the second term by 2. (Hey, this is
      DumBO, not SmartBO!)
     """
-    return y_ex + np.sqrt(y_var) / 2
+    return y_ex + np.sqrt(y_se) / 2
 
 
 def surrogate(x_m, y_m, x):
@@ -50,28 +50,33 @@ def surrogate(x_m, y_m, x):
     i_nn = np.argmin(distance)
     y_ex = y_m[i_nn]
     y_var = distance[i_nn]
-    return y_ex, y_var
+    y_se = np.sqrt(y_var)
+    return y_ex, y_se
 
 
 def _standardize(y):
     return (y - y.mean()) / (1e-9 + y.std())
 
 
-def optimize(x_m, y_m, eps=0.1, num_iterations=1000):
+def optimize(x_m, y_m, eps=0.1, num_iterations=1000, bounds=None):
     """Random search
     Maximize the acquisition function.
     The x value that maximizes the acquisition function
      is the next point to measure.
     Put another way, we design the next experiment
      by maximizing the acquisition function.
+    If you don't specify bounds, we'll just make them [0,1] in
+     all dimensions.
     """
 
     y_m_s = _standardize(y_m)
     num_dim = len(x_m[0])
     x_best = np.random.uniform(size=(num_dim,))
+    if bounds is None:
+        bounds = np.array([[0.0] * num_dim, [1.0] * num_dim])
     af_best = -1e99
     for _ in range(num_iterations):
-        x = np.minimum(1.0, np.maximum(0.0, x_best + eps * np.random.uniform(size=(num_dim,))))
+        x = np.minimum(bounds[1], np.maximum(bounds[0], x_best + eps * np.random.uniform(size=(num_dim,))))
         x = np.atleast_2d(x)
         af = acquisition_function(*surrogate(x_m, y_m_s, x))
         if af > af_best:
--- test_dumbo.py
+++ test_dumbo.py
@@ -3,23 +3,24 @@ import numpy as np
 import dumbo
 
 
-def _test(measure):
+def _test(measure, num_dim):
     np.random.seed(17)  # Make the test stable
-    x_m = np.atleast_2d(np.array([0.5]))
+    x_m = np.atleast_2d(np.array([0.1] * num_dim))
     y_m = np.array([measure(x_m[0])])
 
-    for _ in range(10):
+    for _ in range(40):
         x = dumbo.optimize(x_m, y_m)
         y = measure(x)
         x_m = np.append(x_m, x, axis=0)
         y_m = np.append(y_m, y)
+        print(y_m.max())
 
     assert y_m.max() > -0.01, y_m.max()
 
 
 def test_dumbo_1d():
-    _test(lambda x: float(-((x - 0.3333) ** 2)))
+    _test(lambda x: float(-((x - 0.3333) ** 2)), num_dim=1)
 
 
 def test_dumbo_2d():
-    _test(lambda x: float(-((x - 0.3333) ** 2).sum()))
+    _test(lambda x: float(-((x - 0.3333) ** 2).sum()), num_dim=2)
